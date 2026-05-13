779e9db37e134eb94dffaf146fee6138460da63a
IguanaOstrava <omega.rayfan@gmail.com>
2026-05-13 16:08:10 +0200

    Initial commit

---
+++ src/main/java/com/example/cdpr_game_store/CdprGameStoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.cdpr_game_store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CdprGameStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CdprGameStoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cdpr_game_store/CdprGameStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cdpr_game_store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CdprGameStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
